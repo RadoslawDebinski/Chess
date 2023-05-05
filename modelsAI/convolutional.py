3d4145a9f07c54ff6950389fe9b86e149a0ce638
RadoslawDebinski <83645103+RadoslawDebinski@users.noreply.github.com>
2023-05-05 20:28:43 +0200

    Feature: - dataset creation with threads version 1.3 - AI teaching with tensorflow version 1.0 To Do: - the transformation of the game history to stockfish format for example "e2e4"; - connection of stockfish to proceed move and give it the option to promote the pawn.

--- modelsAI/convolutional.py
+++ modelsAI/convolutional.py
@@ -1,3 +1,5 @@
+import time
+import keras.models
 import numpy as np
 import os
 import tensorflow.keras.models as models
@@ -5,34 +7,50 @@ import tensorflow.keras.layers as layers
 import tensorflow.keras.utils as utils
 import tensorflow.keras.optimizers as optimizers
 import tensorflow.keras.callbacks as callbacks
+import tensorflow.keras.losses as losses
 
 
 class ConvolutionalModelTeaching:
     def __init__(self, dir_path):
+        start_time = time.time()
         self.noFiles = len([entry for entry in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, entry))])
         self.entries = os.listdir(dir_path)
         self.dir_path = dir_path
         self.x_data, self.y_data = self.get_data()
-        # self.x_data = np.concatenate((self.x_data, self.x_data), axis=0)
-        # self.y_data = np.concatenate((self.y_data, self.y_data), axis=0)
-        # self.x_data = np.concatenate((self.x_data, self.x_data), axis=0)
-        # self.y_data = np.concatenate((self.y_data, self.y_data), axis=0)
-        # self.x_data = np.concatenate((self.x_data, self.x_data), axis=0)
-        # self.y_data = np.concatenate((self.y_data, self.y_data), axis=0)
-        # self.x_data = np.concatenate((self.x_data, self.x_data), axis=0)
-        # self.y_data = np.concatenate((self.y_data, self.y_data), axis=0)
-        # self.x_data = np.concatenate((self.x_data, self.x_data), axis=0)
-        # self.y_data = np.concatenate((self.y_data, self.y_data), axis=0)
-        print(f'Packages input shape: {np.shape(self.x_data)}')
-        print(f'Packages output shape: {np.shape(self.y_data)}')
-        # model = self.build_model(32, 4)
-        # model.compile(optimizer=optimizers.Adam(5e-4), loss='mean_squared_error')
-        # model.summary()
-        # model.fit(self.x_data, self.y_data, batch_size=2048, epochs=1000, verbose=1, validation_split=0.1,
-        #           callbacks=[callbacks.ReduceLROnPlateau(monitor='loss', patience=10),
-        #                      callbacks.EarlyStopping(monitor='loss', patience=1000, min_delta=0.0001)])
-        #
-        # model.save('model.h5')
+
+        filePath = f'data_size_13_norm_out\\20230505-174522.npz'
+        with open(filePath, 'rb') as f:
+            self.y_data = np.load(f)
+
+        end_time = time.time()
+        print(f'It took {end_time - start_time} second(s) to load data.')
+
+        model = self.build_model(32, 4)
+        loss = losses.SparseCategoricalCrossentropy()
+        model.compile(optimizer=optimizers.Adam(lr=0.01), loss='mean_squared_error', metrics=["accuracy"])
+        model.summary()
+        model.fit(self.x_data, self.y_data, batch_size=1024, epochs=10, verbose=1, validation_split=0.1,
+                  callbacks=[callbacks.ReduceLROnPlateau(monitor='loss', patience=10),
+                             callbacks.EarlyStopping(monitor='loss', patience=2, min_delta=0.01)])
+
+        model.save('model.h5')
+
+    def normalize_output(self, y_data):
+        positiveBase = np.amax(y_data)
+        negativeBase = np.amin(y_data)
+        negativeBase = 0 - negativeBase
+
+        base = positiveBase + negativeBase
+        new_y_data = y_data + negativeBase
+        new_y_data = new_y_data / base
+
+        filePath = f'data_size_13_norm_out\\{time.strftime("%Y%m%d-%H%M%S")}.npz'
+        with open(filePath, 'wb') as f:
+            np.save(f, new_y_data)
+
+        return np.array(new_y_data)
+
+
 
     def get_data(self):
         x_train = []
@@ -41,25 +59,41 @@ class ConvolutionalModelTeaching:
             with open(f'{dir_path}\\{self.entries[i]}', 'rb') as f:
                 packageInput = np.load(f)
                 packageOutput = np.load(f)
-                print(f'Package {self.entries[i]} input shape: {np.shape(packageInput)}')
-                print(f'Package {self.entries[i]} output shape: {np.shape(packageOutput)}')
                 x_train.append(packageInput)
                 y_train.append(packageOutput)
         return np.concatenate(x_train, axis=0), np.concatenate(y_train, axis=0)
 
     def build_model(self, conv_size, conv_depth):
-        board3d = layers.Input(shape=(4, 8, 8))
-
-        # convolutional layers
+        board3d = layers.Input(shape=(13, 8, 8))
+        x = board3d
         for _ in range(conv_depth):
-            x = layers.Conv2D(filters=conv_size, kernel_size=3, padding='same', activation='sigmoid')(board3d)
-            x = layers.Flatten()(x)
-            x = layers.Dense(64, 'relu')(x)
-            x = layers.Dense(1, 'sigmoid')(x)
+            x = layers.Conv2D(filters=conv_size, kernel_size=3,
+                              padding='same', activation='relu')(x)
+        x = layers.Flatten()(x)
+        x = layers.Dense(64, 'relu')(x)
+        x = layers.Dense(1, 'sigmoid')(x)
+        return models.Model(inputs=board3d, outputs=x)
+
+    def build_model_residual(self, conv_size, conv_depth):
+        board3d = layers.Input(shape=(13, 8, 8))
+
+        # adding the convolutional layers
+        x = layers.Conv2D(filters=conv_size, kernel_size=3, padding='same')(board3d)
+        for _ in range(conv_depth):
+            previous = x
+            x = layers.Conv2D(filters=conv_size, kernel_size=3, padding='same')(x)
+            x = layers.BatchNormalization()(x)
+            x = layers.Activation('relu')(x)
+            x = layers.Conv2D(filters=conv_size, kernel_size=3, padding='same')(x)
+            x = layers.BatchNormalization()(x)
+            x = layers.Add()([x, previous])
+            x = layers.Activation('relu')(x)
+        x = layers.Flatten()(x)
+        x = layers.Dense(1, 'sigmoid')(x)
 
         return models.Model(inputs=board3d, outputs=x)
 
 
 if __name__ == '__main__':
-    dir_path = r'data'
+    dir_path = r'data_size_13'
     ConvolutionalModelTeaching(dir_path)
--- modelsAI/dataCreation.py
+++ modelsAI/dataCreation.py
@@ -1,10 +1,12 @@
+import os
+
 import chess
 import chess.engine
+import itertools
 import time
 import numpy as np
 from threading import Thread
 from gameModes.stockEngine import StockEngine
-
 
 # stockPath = "..\\stockfish-11-win\\Windows\\stockfish_20011801_x64.exe"
 stockPath = "..\\stockfish_15.1_win_x64_avx2\\stockfish-windows-2022-x86-64-avx2.exe"
@@ -30,7 +32,7 @@ class Creator:
             t.join()
 
         end_time = time.time()
-        print(f'It took {end_time- start_time} second(s) to complete.')
+        print(f'It took {end_time - start_time} second(s) to complete.')
 
     def threadFunc(self, threadName):
         for _ in range(self.threadPackages):
@@ -111,5 +113,109 @@ class Creator:
         return board, evaluation
 
 
+class Translator:
+    def __init__(self, sourcePath, destinationPath):
+        self.sourcePath = sourcePath
+        self.destinationPath = destinationPath
+        self.noFiles = len(
+            [entry for entry in os.listdir(sourcePath) if os.path.isfile(os.path.join(sourcePath, entry))])
+        self.entries = os.listdir(sourcePath)
+        start_time = time.time()
+        self.x_data, self.y_data = self.get_data()
+        print(f'Packages input shape: {np.shape(self.x_data)}')
+        print(f'Packages output shape: {np.shape(self.y_data)}')
+
+        self.translate()
+        end_time = time.time()
+        print(f'It took {end_time - start_time} second(s) to complete.')
+
+    def get_data(self):
+        x_train = []
+        y_train = []
+        for i in range(self.noFiles):
+            with open(f'{self.sourcePath}\\{self.entries[i]}', 'rb') as f:
+                packageInput = np.load(f)
+                packageOutput = np.load(f)
+                print(f'Package {self.entries[i]} input shape: {np.shape(packageInput)}')
+                print(f'Package {self.entries[i]} output shape: {np.shape(packageOutput)}')
+                x_train.append(packageInput)
+                y_train.append(packageOutput)
+        return np.concatenate(x_train, axis=0), np.concatenate(y_train, axis=0)
+
+    def translate(self):
+        print(np.shape(self.x_data))
+
+        translation = {
+            (0, 0, 0, 1): [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            (0, 0, 1, 0): [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            (0, 0, 1, 1): [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            (0, 1, 0, 0): [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            (0, 1, 0, 1): [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            (0, 1, 1, 0): [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+
+            (1, 0, 0, 1): [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            (1, 0, 1, 0): [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            (1, 0, 1, 1): [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            (1, 1, 0, 0): [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+            (1, 1, 0, 1): [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            (1, 1, 1, 0): [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+
+            (0, 0, 0, 0): [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        }
+        for iter in range(1000):
+            new_data = np.zeros((1, 13, 8, 8), dtype=np.int8)
+            new_board = np.zeros((13, 8, 8), dtype=np.int8)
+
+            translation_keys = list(translation.keys())
+            translation_values = np.array(list(translation.values()))
+
+            for t, board in enumerate(self.x_data[0+1000*iter:1000+1000*iter], start=1):
+                for i, j in itertools.product(range(8), range(8)):
+                    key = tuple(board[:, i, j])
+                    idx = translation_keys.index(key)
+                    new_board[:, i, j] = translation_values[idx]
+                new_data = np.concatenate((new_data, [new_board]), axis=0)
+                if t % 10000 == 0:
+                    print(f'There is no.{t} boards now.')
+
+            new_data = new_data[1:, :, :, :]
+            self.create_file(new_data, self.y_data)
+
+    def show_visual13(self, board):
+        translation = {
+            (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0): 'P',
+            (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0): 'N',
+            (0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0): 'B',
+            (0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0): 'R',
+            (0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0): 'Q',
+            (0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0): 'K',
+            (0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0): 'p',
+            (0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0): 'n',
+            (0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0): 'b',
+            (0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0): 'r',
+            (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0): 'q',
+            (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0): 'k',
+            (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1): 'v'
+        }
+        translation_keys = list(translation.keys())
+        translation_values = np.array(list(translation.values()))
+
+        new_board = np.chararray((8, 8))
+
+        for i, j in itertools.product(range(8), range(8)):
+            key = tuple(board[:, i, j])
+            idx = translation_keys.index(key)
+            new_board[i, j] = translation_values[idx]
+
+        print(new_board)
+
+    def create_file(self, inputData, outputData):
+        filePath = f'{self.destinationPath}\\data_combined_{time.strftime("%Y%m%d-%H%M%S")}.npz'
+        with open(filePath, 'wb') as f:
+            np.save(f, inputData)
+            np.save(f, outputData)
+
+
 if __name__ == '__main__':
-    Creator('data3')
+    # Creator('data3')
+    Translator('data3', 'data_size_13')
